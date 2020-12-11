7e04041a3a0b544681d500c77f45a414a2096c8d
alexjohnsv <alexjohnsv@gmail.com>
2020-12-12 00:52:34 +0100

    installed redis, built a simple redis client, implemented routes for insert and retrieval

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { encode, decode } = require('./utils/shortener');
+const client = require('./lib/redis-client');
 
 const port = process.env.PORT || 3000;
 
@@ -13,3 +14,21 @@ app.use(express.json());
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
 });
+
+app.post('/api/create', async (req, res) => {
+  const id = await client.asyncIncr('id');
+  const encoded = encode(id);
+
+  const url = req.body.url;
+
+  await client.setAsync(encoded, url);
+
+  return res.json({'url': encoded});
+});
+
+
+app.get('/:s', async (req, res) => {
+  const url = await client.getAsync(req.params.s);
+
+  return res.json({'url': url});
+})
