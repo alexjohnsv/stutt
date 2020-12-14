25aa89c70dd6b41b308346fc6085997801204602
alexjohnsv <alexjohnsv@gmail.com>
2020-12-14 16:25:24 +0100

    basic styling

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const app = express();
 // middlewares
 app.use(cors());
 app.use(express.json());
+app.use(express.static('./public'));
 
 app.post('/api/create', async (req, res) => {
   const id = await client.asyncIncr('id');
